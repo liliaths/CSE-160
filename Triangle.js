185b98c1ea535d2b5609172bee25c8c55c1f97b5
liliaths <124856751+liliaths@users.noreply.github.com>
2025-02-14 22:02:52 -0800

    Add files via upload

--- Triangle.js
+++ Triangle.js
@@ -4,16 +4,6 @@ class Triangle{
     this.position = [0.0, 0.0, 0.0];
     this.color = [1.0, 1.0, 1.0, 1.0];
     this.size = 5.0;
-
-    this.buffer = gl.createBuffer();
-    if (!this.buffer) {
-      console.log('Failed to create the buffer object');
-      return;
-    }
-
-    // Pre-allocate the typed array
-    this.vertices = new Float32Array(9);
-    
   }
 
   render() {
@@ -36,46 +26,10 @@ class Triangle{
   }
 }
 
-var g_vertexBuffer = null;
-function initTriangle3D() {
-
-    // Create a buffer object
-    var vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-      console.log('Failed to create the buffer object');
-      return -1;
-    }
-  
-    // Bind the buffer object to target
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    // Write date into the buffer object
-  
-    // Assign the buffer object to a_Position variable
-    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
-  
-    // Enable the assignment to a_Position variable
-    gl.enableVertexAttribArray(a_Position);
-
-}
-
 function drawTriangle3D(vertices) {
-
-  var n = vertices.length/3; // The number of vertices
-
-  if(g_vertexBuffer == null) { 
-    initTriangle3D();
-  }
-
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
-
-  gl.drawArrays(gl.TRIANGLES, 0, n);
-
-
-
-}
-
-function drawTriangle3DUV(vertices, uv) {
-
+ // var vertices = new Float32Array([
+  //  0, 0.5,   -0.5, -0.5,   0.5, -0.5
+  //]);
   var n = 3; // The number of vertices
 
   // Create a buffer object
@@ -96,26 +50,9 @@ function drawTriangle3DUV(vertices, uv) {
 
   // Enable the assignment to a_Position variable
   gl.enableVertexAttribArray(a_Position);
-
-  var uvBuffer = gl.createBuffer();
-  if(!uvBuffer){
-    console.log('Failed to create the buffer object');
-    return -1;
-  }
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
-
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uv), gl.DYNAMIC_DRAW);
-
-  gl.vertexAttribPointer(a_UV, 2, gl.FLOAT, false, 0, 0);
-
-  gl.enableVertexAttribArray(a_UV);
   
   gl.drawArrays(gl.TRIANGLES, 0, n);
-
-  g_vertexBuffer = null;
-
+  //return n;
 }
-
 
 
